d0ed6aa493f9d466b63bbae94af24eb08a8d54d0
afisman <afisman@gmail.com>
2024-01-27 12:49:53 +0100

    feat: toDoCard, styles and elements

--- src/components/ToDoCard/ToDoCard.jsx
+++ src/components/ToDoCard/ToDoCard.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import './ToDoCard.css';
 
-const ToDoCard = () => {
+const ToDoCard = ({ toDo }) => {
     return (
-        <div>ToDoCard</div>
+        <div className='card'>
+            <h2 className='card__title'>{toDo.description}</h2>
+            <p className='card__priority'>{toDo.priority}</p>
+            <p className='card__deadline'>{(new Date(toDo.deadline)).toDateString()}</p>
+        </div>
     )
 }
 
--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { getToDoList } from '../../features/toDoSlice/toDoSlice';
+import ToDoCard from '../ToDoCard/ToDoCard';
+import './ToDoList.css';
 
 
 const ToDoList = () => {
@@ -16,13 +18,9 @@ const ToDoList = () => {
         <>
             <div>ToDoList</div>
             <button onClick={handleClick}>Add</button>
-            <div>
+            <div className='toDo__list'>
                 {toDoList.map((el, i) => (
-                    <div key={i}>
-                        <h2>{el.description}</h2>
-                        <p>{el.priority}</p>
-                        <p>{(new Date(el.deadline)).toDateString()}</p>
-                    </div>
+                    <ToDoCard toDo={el} key={i} />
                 ))}
             </div>
         </>
